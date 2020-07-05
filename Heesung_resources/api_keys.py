60e2d0774f818d679938dd15bbccc396b92b9c2f
Heesung Shim <heesung80@gmail.com>
2020-07-05 14:47:17 -0700

    “heesung-ETL”

--- Heesung_resources/api_keys.py
+++ Heesung_resources/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "120a4c993e08b8e5529a74a1484ed81c"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyDqdKOFSPtn-QCQXhU_2wG6T1s4ougBBys"
+g_key = ""
